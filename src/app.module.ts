23e1580ad96a0d991f596b065c5298494857b6ff
Afzal <afzalahmeddc@gmail.com>
2026-07-06 16:37:17 +0530

    added supabase connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { config } from '../config/index';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      uri: config.database.uri,
+      dialect: 'postgres',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
